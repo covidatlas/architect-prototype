948da1a2b7c0015b7fda763ae9a7880211e7b4e2
Ryan Block <rblock@gmail.com>
2020-03-16 13:09:15 -0700

    Improve visitor tester script

--- scripts/visitor-tester.js
+++ scripts/visitor-tester.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
+
+// $ ./scripts/visitor-tester.js us ca san-francisco
+
 process.env.NODE_ENV = 'testing'
 let arc = require('@architect/functions')
+let country = process.argv[2] || 'us'
+let region = process.argv[3] || 'ca'
+let locale = process.argv[4] || 'san-francisco'
 
 // Runs the visitor without writing to DB; disable debug to write
 ;(async () => {
   await arc.events.publish({
     name: 'tracker',
     payload: {
-      country: 'us',
-      region: 'ca',
-      locale: 'san-francisco',
+      country,
+      region,
+      locale,
       debug: true
     }
   }
